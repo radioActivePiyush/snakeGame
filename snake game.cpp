7bd781e50a2b5d697a98cc4204aa484eea5be891
PIYUSH VERMA <piyushlionhearts@gmail.com>
2016-10-14 23:32:01 +0530

    print fix and delay added
    delay = 75 sweet

--- snake game.cpp
+++ snake game.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <conio.h>
 #include <cstdlib>
+#include <stdio.h>
+#include<stdlib.h>
+#include <windows.h>
+
 using namespace std;
 bool gameOver;
 const int width = 20 ;
@@ -18,7 +22,7 @@ void setup(){
 	dir = STOP;
 	x = width/2;
 	y = height/2;
-//fruit placement 
+//fruit placement
 	fruitx = rand() % width;
 	fruity = rand() % height;
 	score = 0;
@@ -26,13 +30,14 @@ void setup(){
 }
 void draw(){
 	system("cls");//clear screan
-	for (int i = 0; i < width; ++i)
-		cout <<"@";
-	cout <<endl;
+	for (int i = 0; i < width + 2; ++i)
+    printf("@");
+
+
 
 	for (int i = 0; i < height; ++i)
 	{
-		for (int j = 0; j < width + 2; ++j)
+		for (int j = 0; j < width; ++j)
 		{
 			if (j == 0)
 				cout <<"@";
@@ -45,29 +50,27 @@ void draw(){
 				cout << "y";
 			}
 
-			else 
+			else
 				cout << " ";
 			if (j == width-1)
 			{
 				cout <<"@";
 			}
-			cout <<endl;
-		}
+
+		}cout <<endl;
 	}
+    for (int i = 0; i < width +2; ++i){
+		cout <<'@';
 
-
-
-
-
-	for (int i = 0; i < width; ++i)
-		cout <<"@";
+		}
+    Sleep(60);
 	cout <<endl;
 
-	
+
 
 }
 void input(){
-	
+
 
 }
 void logic(){
