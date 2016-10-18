67fd5f34ac166e6fe290492ba98600a855ba49c4
PIYUSH VERMA <piyushlionhearts@gmail.com>
2016-10-18 19:48:49 +0530

    permeable walls added
    snake can pass through the walls , the only thing that can kill the
    snake is its own tail

--- snake game.cpp
+++ snake game.cpp
@@ -133,8 +133,13 @@ void logic()
 				y++;
 				break;
 			}
-	if (x>width || x<0 || y >height|| y<0)
-		gameOver = true;
+	//if (x>width || x<0 || y >height|| y<0)
+	//	gameOver = true;
+
+    if (x>=width)x= 0;
+    else if (x<0) x= width-1;
+    if (y>= height)y=0;
+    else if (y<0)y = height-1;
 
     for(int i = 0;i<nTail;i++){
         if (tailX[i] == x && tailY[i]== y)
